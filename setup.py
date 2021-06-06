86643844a6609c4e341e9333d343ed48f8166d9e
pde-bakk <pde-bakk@student.codam.nl>
2021-06-06 22:24:27 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='DSLR',
+	version='1.0.0',
+	packages=['pkgs'],
+	url='https://github.com/pde-bakk/DSLR.git',
+	license='',
+	author='pde-bakk',
+	author_email='pde-bakk@student.codam.nl',
+	description='Harry Potter and the Data Scientist'
+)
