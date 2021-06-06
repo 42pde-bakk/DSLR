66392f1be03851680176c7236b2aba8d5b2ccc74
pde-bakk <pde-bakk@student.codam.nl>
2021-06-06 20:55:04 +0200

    added check_input method for predicting

--- V3/logreg_predict.py
+++ V3/logreg_predict.py
@@ -1,17 +1,10 @@
+from pkgs.parsing import predict_check_input
 import pandas as pd
 import numpy as np
 import sys
-import os
 import pickle
 
-if len(sys.argv) != 3 or 'dataset_test.csv' not in sys.argv[1]:
-	print('It takes as a parameter dataset_test.csv and a file containing the weights trained by previous program.')
-	quit()
-
-filename, extension = os.path.splitext(sys.argv[1])
-if extension != '.csv' or not os.path.exists(sys.argv[1]):
-	print(f'Please provide a valid .csv file.')
-	quit()
+predict_check_input(sys.argv)
 
 df = pd.read_csv(sys.argv[1], index_col=0)
 
--- pkgs/parsing.py
+++ pkgs/parsing.py
@@ -10,3 +10,14 @@ def check_input(argv):
     if extension != '.csv' or not os.path.exists(argv[1]):
         print(f'Please provide a valid .csv file.')
         quit()
+
+
+def predict_check_input(argv):
+    if len(argv) != 3 or 'dataset_test.csv' not in argv[1]:
+        print('It takes as a parameter dataset_test.csv and a file containing the weights trained by previous program.')
+        quit()
+
+    filename, extension = os.path.splitext(argv[1])
+    if extension != '.csv' or not os.path.exists(argv[1]):
+        print(f'Please provide a valid .csv file.')
+        quit()
