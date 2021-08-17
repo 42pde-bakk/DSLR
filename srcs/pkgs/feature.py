f3d24492bd23c48bed1c1351a8217e401165090c
Peer de Bakker <pde-bakk@student.codam.nl>
2021-08-17 16:57:56 +0200

    split up std deviation calculation

--- srcs/pkgs/feature.py
+++ srcs/pkgs/feature.py
@@ -1,6 +1,11 @@
 import math
 
 
+def std_deviation(items) -> float:
+    mean = sum(items) / len(items)
+    return math.sqrt(sum([float((float(x) - mean) ** 2) for x in items]) / len(items))
+
+
 class Feature:
     def __init__(self, n, col):
         self.name = n
@@ -9,7 +14,7 @@ class Feature:
         col.sort()
         self.min, self.max = col[0], col[-1]
         self.p25, self.p50, self.p75 = col[int(len(col) / 4)], col[int(len(col) / 2)], col[int(len(col) / 4 * 3)]
-        self.std = math.sqrt(sum([float((float(x) - self.mean) ** 2) for x in col]) / self.count)
+        self.std = std_deviation(col)
 
     def getvalue(self, val):
         return {
