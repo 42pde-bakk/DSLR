222a59a1077f901bb164a1c873584a6338d16b4a
Peer de Bakker <pde-bakk@student.codam.nl>
2021-09-21 12:09:51 +0200

    removed cleveland heart disease test data

--- srcs/logreg_train.py
+++ srcs/logreg_train.py
@@ -1,51 +1,8 @@
 from pkgs.parsing import check_input
 from pkgs.logisticregression import LogisticRegression
+from pkgs.weights import save_weights
 import pandas as pd
-import numpy as np
 import sys
-
-
-def print_result(y_test, y_pred):
-	correct = 0
-	for test, pred in zip(y_test, y_pred):
-		if test == pred:
-			correct += 1
-	print(f'Got {correct}/{len(y_test)} correct!')
-
-
-def parse_data():
-	col_names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'num']  # column names
-
-	df = pd.read_csv('datasets/processed.cleveland.data.csv', header=None)
-	df.columns = col_names  # setting dataframe column names
-	df.replace({'?': np.nan}, inplace=True)
-	df[['ca', 'thal']] = df[['ca', 'thal']].astype('float64')  # Casting columns data-type to floats
-	df['ca'].replace({np.nan: df['ca'].median()}, inplace=True)  # replaces null values of ca column with median value
-	df['thal'].replace({np.nan: df['thal'].median()}, inplace=True)
-	return df
-
-
-def split_dataset(df):
-	# selecting all the features within our dataset
-	features = df[
-		['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
-	]
-	features = features.to_numpy()  # converts feature set to numpy array
-	target = df['num'].to_numpy()  # converts target column to numpy array
-	print(features.shape, len(target))
-	return features, target
-
-
-def test(df):
-	lr = LogisticRegression()
-	x_train, y_train, x_test, y_test = lr.train_test_split(df)
-	# print(f'shapes:', x_train.shape, y_train.shape, x_test.shape, y_test.shape)
-	loss_list = lr.fit(x_train, y_train)
-
-	test_preds = lr.predict(x_test)
-	acc = lr.accuracy(test_preds, y_test)
-	print(acc)
-	return acc
 
 
 def harrypotter():
@@ -54,36 +11,19 @@ def harrypotter():
 	df = pd.read_csv(sys.argv[1], index_col=0)
 	df.drop(labels=['First Name', 'Last Name', 'Birthday', 'Best Hand'], inplace=True, axis=1)
 	df.fillna(0, inplace=True)  # Fill all NaN's with 0's
-	# df.replace({'Ravenclaw': 0, 'Slytherin': 1, 'Gryffindor': 2, 'Hufflepuff': 3}, inplace=True)
 
 	y = df['Hogwarts House'].to_numpy()
 	df.drop('Hogwarts House', inplace=True, axis=1)
 	x = df.to_numpy()
 
-	train_x, train_y, test_x, test_y = LogisticRegression.train_test_split(x, y, test_size=0.2)
-	lr = LogisticRegression(n_iterations=100)
-	loss_list = lr.fit(train_x, train_y)
-	print(f'loss_list={loss_list}, {len(loss_list)}')
+	lr = LogisticRegression(n_iterations=500)
+	train_x, train_y, test_x, test_y = lr.train_test_split(x, y, test_size=0.2)
+	lr.fit(train_x, train_y)
 	test_preds = lr.predict(test_x)
 	acc = lr.accuracy(test_preds, test_y)
-	print(f'accuracy is {acc}%')
-	# save_weights(model)
+	save_weights(lr, 'datasets/weights.csv')
 	return acc
 
 
-def mymain(count=10):
-	df = parse_data()
-	total = 0
-	for i in range(count):
-		total += harrypotter()
-	print(f'average = {total / count}')
-
-	# features, target = split_dataset(df)
-	# lr = LogisticRegression()
-	# standardized_features = LogisticRegression.standardize_data(features)
-	# probs, preds = lr.multinomialLogReg(standardized_features, target)
-	# print(lr.accuracy(preds, target))
-
-
 if __name__ == '__main__':
-	mymain(1)
+	harrypotter()
