6e644242e3f4447625e1887fab8dbef0beba602e
Peer de Bakker <pde-bakk@student.codam.nl>
2021-09-24 17:19:22 +0200

    im lazy

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pip3 install -r requirements.txt > /dev/null
+
+python3 srcs/logreg_train.py datasets/dataset_train.csv
+python3 srcs/logreg_predict.py datasets/dataset_test.csv datasets/weights
+
+python3 secret/evaluate.py
