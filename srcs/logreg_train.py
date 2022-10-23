6e47ce957777ea6b85bc56affee6744edd34f8db
Restyled.io <commits@restyled.io>
2022-10-23 15:35:20 +0000

    Restyled by autopep8

--- srcs/logreg_train.py
+++ srcs/logreg_train.py
@@ -10,9 +10,9 @@ import numpy as np
 import sys
 
 FEATURES = [
-	'Arithmancy', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Divination', 'Muggle Studies',
-	'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions', 'Care of Magical Creatures', 'Charms',
-	'Flying'
+    'Arithmancy', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Divination', 'Muggle Studies',
+    'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions', 'Care of Magical Creatures', 'Charms',
+    'Flying'
 ]
 
 TARGET_COLUMN = 'Hogwarts House'
@@ -22,39 +22,40 @@ TARGET_COLUMN = 'Hogwarts House'
 
 
 def combine_models(models: list[MyLogR], x_test: np.ndarray, y_test: np.ndarray, houses_dict: dict):
-	predict_together = np.hstack([m.predict_(x_test) for m in models])
-	# for i in range(len(models)):
-	# 	print(predict_together[i][0])
-	y_hat = predict_together.argmax(axis=1).reshape(-1, 1)
-	# u, inv = np.unique(y_hat, return_inverse=True)
-	print(f'accuracy = {accuracy_score_(y_test, y_hat) * 100:.1f}%')
+    predict_together = np.hstack([m.predict_(x_test) for m in models])
+    # for i in range(len(models)):
+    # 	print(predict_together[i][0])
+    y_hat = predict_together.argmax(axis=1).reshape(-1, 1)
+    # u, inv = np.unique(y_hat, return_inverse=True)
+    print(f'accuracy = {accuracy_score_(y_test, y_hat) * 100:.1f}%')
 
 
 def harrypotter():
-	check_input(sys.argv)
+    check_input(sys.argv)
 
-	df = pd.read_csv(sys.argv[1], index_col=0)
-	df.fillna(0.0, inplace=True)
+    df = pd.read_csv(sys.argv[1], index_col=0)
+    df.fillna(0.0, inplace=True)
 
-	y = df[TARGET_COLUMN].to_numpy().reshape(-1, 1)
-	x = df[FEATURES].to_numpy().reshape(-1, len(FEATURES))
-	x = (x - x.mean(axis=0)) / x.std(axis=0)  # normalize the data
-	unique_houses = np.unique(y)
-	houses_dict = {house: idx for idx, house in enumerate(unique_houses)}
-	for k, v in houses_dict.items():
-		y[y == k] = v
-	train_x, test_x, train_y, test_y = data_splitter(x, y, 0.8)
-	models = []
+    y = df[TARGET_COLUMN].to_numpy().reshape(-1, 1)
+    x = df[FEATURES].to_numpy().reshape(-1, len(FEATURES))
+    x = (x - x.mean(axis=0)) / x.std(axis=0)  # normalize the data
+    unique_houses = np.unique(y)
+    houses_dict = {house: idx for idx, house in enumerate(unique_houses)}
+    for k, v in houses_dict.items():
+        y[y == k] = v
+    train_x, test_x, train_y, test_y = data_splitter(x, y, 0.8)
+    models = []
 
-	for i, house in enumerate(unique_houses):
-		# Train a model for each house (One vs All)
-		print(f'Let\'s train model {i} for {house}')
-		model = MyLogR(thetas=np.ones(shape=(len(FEATURES) + 1, 1)), alpha=0.0001, max_iter=20_000)
-		new_train_y = np.where(train_y == i, 1, 0)
-		model.fit_(train_x, new_train_y)
-		models.append(copy.deepcopy(model))
-	combine_models(models, test_x, test_y, houses_dict)
+    for i, house in enumerate(unique_houses):
+        # Train a model for each house (One vs All)
+        print(f'Let\'s train model {i} for {house}')
+        model = MyLogR(thetas=np.ones(shape=(len(FEATURES) + 1, 1)),
+                       alpha=0.0001, max_iter=20_000)
+        new_train_y = np.where(train_y == i, 1, 0)
+        model.fit_(train_x, new_train_y)
+        models.append(copy.deepcopy(model))
+    combine_models(models, test_x, test_y, houses_dict)
 
 
 if __name__ == '__main__':
-	harrypotter()
+    harrypotter()
--- srcs/pkgs/other_metrics.py
+++ srcs/pkgs/other_metrics.py
@@ -2,114 +2,114 @@ import numpy as np
 
 
 def accepts(*types):
-	def check_accepts(f):
-		if len(types) != f.__code__.co_argcount:
-			return None
+    def check_accepts(f):
+        if len(types) != f.__code__.co_argcount:
+            return None
 
-		def new_f(*args, **kwargs):
-			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
-				return None
-			return f(*args, **kwargs)
+        def new_f(*args, **kwargs):
+            if any(not isinstance(arg, t) for arg, t in zip(args, types)):
+                return None
+            return f(*args, **kwargs)
 
-		# new_f.__name__ = f.__name__
-		return new_f
+        # new_f.__name__ = f.__name__
+        return new_f
 
-	return check_accepts
+    return check_accepts
 
 
 # noinspection PyTypeChecker
 def true_positives(y: np.ndarray, y_hat: np.ndarray, pos_label: int = 1) -> int:
-	return np.sum((y == pos_label) & (y_hat == pos_label))
+    return np.sum((y == pos_label) & (y_hat == pos_label))
 
 
 # noinspection PyTypeChecker
 def true_negatives(y: np.ndarray, y_hat: np.ndarray, pos_label: int = 1) -> int:
-	return np.sum((y != pos_label) & (y_hat != pos_label))
+    return np.sum((y != pos_label) & (y_hat != pos_label))
 
 
 # noinspection PyTypeChecker
 def false_positives(y: np.ndarray, y_hat: np.ndarray, pos_label: int = 1) -> int:
-	return np.sum((y != pos_label) & (y_hat == pos_label))
+    return np.sum((y != pos_label) & (y_hat == pos_label))
 
 
 # noinspection PyTypeChecker
 def false_negatives(y: np.ndarray, y_hat: np.ndarray, pos_label: int = 1) -> int:
-	return np.sum((y == pos_label) & (y_hat != pos_label))
+    return np.sum((y == pos_label) & (y_hat != pos_label))
 
 
 @accepts(np.ndarray, np.ndarray)
 def accuracy_score_(y: np.ndarray, y_hat: np.ndarray) -> float:
-	"""
-	Compute the accuracy score.
-	Args:
-	y:a numpy.ndarray for the correct labels
-	y_hat:a numpy.ndarray for the predicted labels
-	Returns:
-	The accuracy score as a float.
-	None on any error.
-	Raises:
-	This function should not raise any Exception.
-	"""
-	return np.sum(y == y_hat) / y.shape[0]
+    """
+    Compute the accuracy score.
+    Args:
+    y:a numpy.ndarray for the correct labels
+    y_hat:a numpy.ndarray for the predicted labels
+    Returns:
+    The accuracy score as a float.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    return np.sum(y == y_hat) / y.shape[0]
 
 
 @accepts(np.ndarray, np.ndarray, (int, str))
 def precision_score_(y: np.ndarray, y_hat: np.ndarray, pos_label=1) -> float:
-	"""
-	Compute the precision score.
-	Args:
-	y:a numpy.ndarray for the correct labels
-	y_hat:a numpy.ndarray for the predicted labels
-	pos_label: str or int, the class on which to report the precision_score (default=1)
-	Return:
-	The precision score as a float.
-	None on any error.
-	Raises:
-	This function should not raise any Exception.
-	"""
-	tp = true_positives(y, y_hat, pos_label)
-	fp = false_positives(y, y_hat, pos_label)
-	return tp / (tp + fp)
+    """
+    Compute the precision score.
+    Args:
+    y:a numpy.ndarray for the correct labels
+    y_hat:a numpy.ndarray for the predicted labels
+    pos_label: str or int, the class on which to report the precision_score (default=1)
+    Return:
+    The precision score as a float.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    tp = true_positives(y, y_hat, pos_label)
+    fp = false_positives(y, y_hat, pos_label)
+    return tp / (tp + fp)
 
 
 @accepts(np.ndarray, np.ndarray, (int, str))
 def recall_score_(y: np.ndarray, y_hat: np.ndarray, pos_label=1) -> float:
-	"""
-	Compute the recall score.
-	Args:
-	y:a numpy.ndarray for the correct labels
-	y_hat:a numpy.ndarray for the predicted labels
-	pos_label: str or int, the class on which to report the precision_score (default=1)
-	Return:
-	The recall score as a float.
-	None on any error.
-	Raises:
-	This function should not raise any Exception.
-	"""
-	tp = true_positives(y, y_hat, pos_label)
-	fn = false_negatives(y, y_hat, pos_label)
-	return tp / (tp + fn)
+    """
+    Compute the recall score.
+    Args:
+    y:a numpy.ndarray for the correct labels
+    y_hat:a numpy.ndarray for the predicted labels
+    pos_label: str or int, the class on which to report the precision_score (default=1)
+    Return:
+    The recall score as a float.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    tp = true_positives(y, y_hat, pos_label)
+    fn = false_negatives(y, y_hat, pos_label)
+    return tp / (tp + fn)
 
 
 @accepts(np.ndarray, np.ndarray, (int, str))
 def f1_score_(y: np.ndarray, y_hat: np.ndarray, pos_label=1) -> float:
-	"""
-	Compute the f1 score.
-	Args:
-	y:a numpy.ndarray for the correct labels
-	y_hat:a numpy.ndarray for the predicted labels
-	pos_label: str or int, the class on which to report the precision_score (default=1)
-	Returns:
-	The f1 score as a float.
-	None on any error.
-	Raises:
-	This function should not raise any Exception.
-	"""
-	tp = true_positives(y, y_hat, pos_label)
-	fp = false_positives(y, y_hat, pos_label)
-	fn = false_negatives(y, y_hat, pos_label)
-	tn = true_negatives(y, y_hat, pos_label)
-	print(f'{tp=},{fp=},{fn=},{tn=}')
-	precision = precision_score_(y, y_hat, pos_label)
-	recall = recall_score_(y, y_hat, pos_label)
-	return (2 * precision * recall) / (precision + recall)
+    """
+    Compute the f1 score.
+    Args:
+    y:a numpy.ndarray for the correct labels
+    y_hat:a numpy.ndarray for the predicted labels
+    pos_label: str or int, the class on which to report the precision_score (default=1)
+    Returns:
+    The f1 score as a float.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    tp = true_positives(y, y_hat, pos_label)
+    fp = false_positives(y, y_hat, pos_label)
+    fn = false_negatives(y, y_hat, pos_label)
+    tn = true_negatives(y, y_hat, pos_label)
+    print(f'{tp=},{fp=},{fn=},{tn=}')
+    precision = precision_score_(y, y_hat, pos_label)
+    recall = recall_score_(y, y_hat, pos_label)
+    return (2 * precision * recall) / (precision + recall)
