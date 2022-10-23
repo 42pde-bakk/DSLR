54e1d13283a58190c28ffc59ba44abc2fe4ec94b
pde-bakk <pde-bakk@student.codam.nl>
2022-10-23 17:28:01 +0200

    redid the training using my things from the machine learning modules, improved my understanding a ton

--- srcs/logreg_train.py
+++ srcs/logreg_train.py
@@ -1,28 +1,59 @@
+import copy
+
 from pkgs.parsing import check_input
-from pkgs.logisticregression import LogisticRegression
+from pkgs.my_logistic_regression import MyLogisticRegression as MyLogR
+from pkgs.data_splitter import data_splitter
+from pkgs.other_metrics import accuracy_score_, f1_score_
 from pkgs.weights import save_weights
 import pandas as pd
+import numpy as np
 import sys
+
+FEATURES = [
+	'Arithmancy', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Divination', 'Muggle Studies',
+	'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions', 'Care of Magical Creatures', 'Charms',
+	'Flying'
+]
+
+TARGET_COLUMN = 'Hogwarts House'
+# FEATURES = [
+# 	'Care of Magical Creatures', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts'
+# ]
+
+
+def combine_models(models: list[MyLogR], x_test: np.ndarray, y_test: np.ndarray, houses_dict: dict):
+	predict_together = np.hstack([m.predict_(x_test) for m in models])
+	# for i in range(len(models)):
+	# 	print(predict_together[i][0])
+	y_hat = predict_together.argmax(axis=1).reshape(-1, 1)
+	# u, inv = np.unique(y_hat, return_inverse=True)
+	print(f'accuracy = {accuracy_score_(y_test, y_hat) * 100:.1f}%')
 
 
 def harrypotter():
 	check_input(sys.argv)
 
 	df = pd.read_csv(sys.argv[1], index_col=0)
-	df.drop(labels=['First Name', 'Last Name', 'Birthday', 'Best Hand'], inplace=True, axis=1)
-	df.fillna(0, inplace=True)  # Fill all NaN's with 0's
+	df.fillna(0.0, inplace=True)
 
-	y = df['Hogwarts House'].to_numpy()
-	df.drop('Hogwarts House', inplace=True, axis=1)
-	x = df.to_numpy()
+	y = df[TARGET_COLUMN].to_numpy().reshape(-1, 1)
+	x = df[FEATURES].to_numpy().reshape(-1, len(FEATURES))
+	x = (x - x.mean(axis=0)) / x.std(axis=0)  # normalize the data
+	unique_houses = np.unique(y)
+	houses_dict = {house: idx for idx, house in enumerate(unique_houses)}
+	for k, v in houses_dict.items():
+		y[y == k] = v
+	train_x, test_x, train_y, test_y = data_splitter(x, y, 0.8)
+	models = []
 
-	lr = LogisticRegression(n_iterations=100)
-	train_x, train_y, test_x, test_y = lr.train_test_split(x, y, test_size=0.2)
-	lr.fit(train_x, train_y)
-	test_preds = lr.predict(test_x)
-	acc = lr.accuracy(test_preds, test_y)
-	save_weights(lr, 'datasets/weights.csv')
-	return acc
+	for i, house in enumerate(unique_houses):
+		# Train a model for each house (One vs All)
+		print(f'Let\'s train model {i} for {house}')
+		model = MyLogR(thetas=np.ones(shape=(len(FEATURES) + 1, 1)), alpha=0.0001, max_iter=20_000)
+		new_train_y = np.where(train_y == i, 1, 0)
+		model.fit_(train_x, new_train_y)
+		models.append(copy.deepcopy(model))
+	combine_models(models, test_x, test_y, houses_dict)
 
 
 if __name__ == '__main__':
---
+++ srcs/pkgs/my_logistic_regression.py
@@ -0,0 +1,143 @@
+import numpy as np
+
+
+def accepts(*types):
+	def check_accepts(f):
+		if len(types) != f.__code__.co_argcount:
+			return None
+
+		def new_f(*args, **kwargs):
+			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
+				return None
+			return f(*args, **kwargs)
+
+		# new_f.__name__ = f.__name__
+		return new_f
+
+	return check_accepts
+
+
+class MyLogisticRegression:
+	"""
+	Description: My personal logistic regression to classify things.
+	If a function has the __ prefix, it means that it assumes the x value has a column of ones already...
+	"""
+
+	def __init__(self, thetas: np.ndarray, alpha: float = 0.005, max_iter: int = 10_000):
+		if not isinstance(thetas, np.ndarray) or not isinstance(alpha, float) or not isinstance(max_iter, int):
+			raise TypeError('Bad arguments given to MyLogisticRegression')
+		self.alpha = alpha
+		self.max_iter = max_iter
+		self.thetas = thetas
+
+	@staticmethod
+	@accepts(np.ndarray)
+	def sigmoid_(x: np.ndarray) -> np.ndarray:
+		"""
+		Compute the sigmoid of a vector.
+		Args:
+		x: has to be a numpy.ndarray of shape (m, 1).
+		Returns:
+		The sigmoid value as a numpy.ndarray of shape (m, 1).
+		None if x is an empty numpy.ndarray.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		return 1 / (1 + np.exp(-x))
+
+	def gradient_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+		"""Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatiblArgs:
+		x: has to be an numpy.ndarray, a matrix of shape m * n.
+		y: has to be an numpy.ndarray, a vector of shape m * 1.
+		theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
+		Returns:
+		The gradient as a numpy.ndarray, a vector of shape n * 1, containing the result of the formula for all j.
+		None if x, y, or theta are empty numpy.ndarray.
+		None if x, y and theta do not have compatible dimensions.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		ones = np.ones(shape=(x.shape[0], 1))
+		x = np.hstack((ones, x))
+		y_hat = self.__predict_(x)
+		return x.T.dot(y_hat - y) / y.shape[0]
+
+	def __gradient_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+		y_hat = self.__predict_(x)
+		return x.T.dot(y_hat - y) / y.shape[0]
+
+	def fit_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+		"""
+		:param x: np.ndarray
+		:param y: np.ndarray
+		:return: new theta values
+		"""
+		x = np.column_stack((np.ones(shape=(x.shape[0], 1)), x))
+		for idx in range(self.max_iter):
+			assert not np.isnan(self.__gradient_(x, y)).any()
+			self.thetas -= (self.alpha * self.__gradient_(x, y))
+		return self.thetas
+
+	@staticmethod
+	@accepts(np.ndarray, np.ndarray, float)
+	def loss_elem_(y: np.ndarray, y_hat: np.ndarray, eps: float = 1e-15) -> np.ndarray | None:
+		"""
+		:param y: Actual values as an np.ndarray
+		:param y_hat: Predicted values as an np.ndarray
+		:param eps: very small value
+		:return: np.ndarray of the losses
+		"""
+		if y.shape != y_hat.shape:
+			return None
+		return y * np.log(y_hat + eps) + (1 - y) * np.log(1 - y_hat + eps)
+
+	@staticmethod
+	@accepts(np.ndarray, np.ndarray, float)
+	def loss_(y: np.ndarray, y_hat: np.ndarray, eps: float = 1e-15) -> float:
+		"""
+		Computes the logistic loss value.
+		Args:
+		y: has to be an numpy.ndarray, a vector of shape m * 1.
+		y_hat: has to be an numpy.ndarray, a vector of shape m * 1.
+		eps: has to be a float, epsilon (default=1e-15)
+		Returns:
+		The logistic loss value as a float.
+		None on any error.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		loss_elem = MyLogisticRegression.loss_elem_(y, y_hat, eps)
+		return -loss_elem.sum() / y.shape[0]
+
+	def __predict_(self, x: np.ndarray) -> np.ndarray:
+		"""Computes the vector of prediction y_hat from two non-empty numpy.ndarray.
+		Args:
+		x: has to be an numpy.ndarray, a vector of dimension m * n.
+		theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
+		Returns:
+		y_hat as a numpy.ndarray, a vector of dimension m * 1.
+		None if x or theta are empty numpy.ndarray.
+		None if x or theta dimensions are not appropriate.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		assert not np.isnan(x.dot(self.thetas)).any()
+		return MyLogisticRegression.sigmoid_(x.dot(self.thetas))
+
+	def predict_(self, x: np.ndarray) -> np.ndarray | None:
+		"""Computes the vector of prediction y_hat from two non-empty numpy.ndarray.
+		Args:
+		x: has to be an numpy.ndarray, a vector of dimension m * n.
+		theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
+		Returns:
+		y_hat as a numpy.ndarray, a vector of dimension m * 1.
+		None if x or theta are empty numpy.ndarray.
+		None if x or theta dimensions are not appropriate.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		if not isinstance(x, np.ndarray) or x.size == 0:
+			return None
+		ones = np.ones(shape=(x.shape[0], 1))
+		x = np.hstack((ones, x))
+		return MyLogisticRegression.sigmoid_(x.dot(self.thetas))
